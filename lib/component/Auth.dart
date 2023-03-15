c4236263f326e1683fdf22a8562bee282c012083
vishal_Kumar <vishalk74064@gmail.com>
2023-03-15 17:09:24 +0530

    first commit of day 15/03/2023

--- lib/component/Auth.dart
+++ lib/component/Auth.dart
@@ -1,27 +1,27 @@
-// ignore_for_file: sort_child_properties_last
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'dart:ui';
 
 class SignupForm extends StatefulWidget {
   final void Function(String email, String password, bool islogin) submitfm;
-  SignupForm(this.submitfm);
+  // ignore: use_key_in_widget_constructors
+  const SignupForm(this.submitfm);
   @override
   State<SignupForm> createState() => SignupFormState();
 }
 
 class SignupFormState extends State<SignupForm> {
   final _formkey = GlobalKey<FormState>();
-  late final String userEmail;
-  late final String userPass;
+  var userEmail="";
+  var userPass="";
+  var islogin=true;
   void trysubmit() {
     FocusScope.of(context).unfocus();
     final isvalid = _formkey.currentState?.validate();
     if (isvalid != null) {
       _formkey.currentState?.save();
     }
-    widget.submitfm(userEmail, userPass, false);
+    widget.submitfm(userEmail, userPass, islogin);
   }
 
   @override
@@ -36,8 +36,8 @@ class SignupFormState extends State<SignupForm> {
         ),
         child: Center(
           child: Container(
-            width: 300,
-            height: 400,
+            width: 320,
+            height: 420,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(16),
               boxShadow: [
@@ -61,150 +61,155 @@ class SignupFormState extends State<SignupForm> {
                       color: Colors.white.withOpacity(0.2),
                     ),
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Form(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "SIGNUP NOW!",
-                            style: GoogleFonts.poppins(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          ),
-                          const SizedBox(height: 30),
-                          TextFormField(
-                            validator: (value) {
-                              if (value == null ||
-                                  !value.contains('@') ||
-                                  !value.contains('.')) {
-                                return "please enter valid email";
-                              }
-                              return null;
-                            },
-                            decoration: InputDecoration(
-                              labelText: 'Email',
-                              hintText: 'Enter your email',
-                              hintStyle: GoogleFonts.poppins(
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Form(
+                        key: _formkey,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "SIGNUP NOW!",
+                              style: GoogleFonts.poppins(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
                                 color: Colors.white,
                               ),
-                              labelStyle: GoogleFonts.poppins(
-                                color: Colors.white.withOpacity(0.5),
-                              ),
-                              filled: true,
-                              fillColor: Colors.white.withOpacity(0.1),
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12),
-                                borderSide: BorderSide(
-                                  color: Colors.white.withOpacity(0.1),
-                                  width: 0,
-                                ),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12),
-                                borderSide: BorderSide(
-                                  color: Colors.white.withOpacity(0.3),
-                                  width: 2,
-                                ),
-                              ),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12),
-                                borderSide: BorderSide(
-                                  color: Colors.white.withOpacity(0.1),
-                                  width: 0,
-                                ),
-                              ),
                             ),
-                            style: GoogleFonts.poppins(
-                              color: Colors.white,
-                            ),
-                            onSaved: (newValue) =>
-                                userEmail = newValue as String,
-                          ),
-                          const SizedBox(height: 20),
-                          TextFormField(
-                            validator: (value) {
-                              if (value == null || value.length < 8) {
-                                return "please enter password with more than 8 characters";
-                              }
-                              return null;
-                            },
-                            obscureText: true,
-                            decoration: InputDecoration(
-                              hintText: 'Enter your password',
-                              labelText: 'Password',
-                              hintStyle: GoogleFonts.poppins(
+                            const SizedBox(height: 30),
+                            TextFormField(
+                              validator: (value) {
+                                if (value == null ||
+                                    !value.contains('@') ||
+                                    !value.contains('.')) {
+                                  return "please enter valid email";
+                                }
+                                return null;
+                              },
+                              decoration: InputDecoration(
+                                labelText: 'Email',
+                                hintText: 'Enter your email',
+                                hintStyle: GoogleFonts.poppins(
+                                  color: Colors.white,
+                                ),
+                                labelStyle: GoogleFonts.poppins(
+                                  color: Colors.white.withOpacity(0.5),
+                                ),
+                                filled: true,
+                                fillColor: Colors.white.withOpacity(0.1),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                  borderSide: BorderSide(
+                                    color: Colors.white.withOpacity(0.1),
+                                    width: 0,
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                  borderSide: BorderSide(
+                                    color: Colors.white.withOpacity(0.3),
+                                    width: 2,
+                                  ),
+                                ),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                  borderSide: BorderSide(
+                                    color: Colors.white.withOpacity(0.1),
+                                    width: 0,
+                                  ),
+                                ),
+                              ),
+                              style: GoogleFonts.poppins(
                                 color: Colors.white,
                               ),
-                              labelStyle: GoogleFonts.poppins(
-                                color: Colors.white.withOpacity(0.5),
-                              ),
-                              filled: true,
-                              fillColor: Colors.white.withOpacity(0.1),
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12),
-                                borderSide: BorderSide(
-                                  color: Colors.white.withOpacity(0.1),
-                                  width: 0,
-                                ),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12),
-                                borderSide: BorderSide(
-                                  color: Colors.white.withOpacity(0.3),
-                                  width: 2,
-                                ),
-                              ),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12),
-                                borderSide: BorderSide(
-                                  color: Colors.white.withOpacity(0.1),
-                                  width: 0,
-                                ),
-                              ),
+                              onSaved: (Value) => userEmail = Value as String,
                             ),
-                            style: GoogleFonts.poppins(
-                              color: Colors.white,
-                            ),
-                            onSaved: (newValue) =>
-                                userPass = newValue as String,
-                          ),
-                          const SizedBox(height: 20),
-                          GestureDetector(
-                            onTap: () {},
-                            child: Container(
-                              height: 45,
-                              decoration: BoxDecoration(
-                                color: Colors.white.withOpacity(0.5),
-                                borderRadius: BorderRadius.circular(10),
+                            const SizedBox(height: 20),
+                            TextFormField(
+                              validator: (value) {
+                                if (value == null || value.length < 8) {
+                                  return "please enter password with more than 8 characters";
+                                }
+                                return null;
+                              },
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                hintText: 'Enter your password',
+                                labelText: 'Password',
+                                hintStyle: GoogleFonts.poppins(
+                                  color: Colors.white,
+                                ),
+                                labelStyle: GoogleFonts.poppins(
+                                  color: Colors.white.withOpacity(0.5),
+                                ),
+                                filled: true,
+                                fillColor: Colors.white.withOpacity(0.1),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                  borderSide: BorderSide(
+                                    color: Colors.white.withOpacity(0.1),
+                                    width: 0,
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                  borderSide: BorderSide(
+                                    color: Colors.white.withOpacity(0.3),
+                                    width: 2,
+                                  ),
+                                ),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                  borderSide: BorderSide(
+                                    color: Colors.white.withOpacity(0.1),
+                                    width: 0,
+                                  ),
+                                ),
                               ),
-                              child: Center(
-                                child: Text(
-                                  "Sign Up",
-                                  style: GoogleFonts.poppins(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                    color: Color.fromARGB(255, 8, 4, 113),
+                              style: GoogleFonts.poppins(
+                                color: Colors.white,
+                              ),
+                              onSaved: (Value) => userPass = Value as String,
+                            ),
+                            const SizedBox(height: 20),
+                            GestureDetector(
+                              onTap: trysubmit,
+                              child: Container(
+                                height: 45,
+                                decoration: BoxDecoration(
+                                  color: Colors.white.withOpacity(0.5),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    islogin?"Login":"Sign Up",
+                                    style: GoogleFonts.poppins(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: const Color.fromARGB(255, 8, 4, 113),
+                                    ),
                                   ),
                                 ),
                               ),
                             ),
-                          ),
-                          SizedBox(height: 5),
-                          TextButton(
-                            onPressed: () {},
-                            child: Text(
-                              "Already have an account? Log in here",
-                              style: GoogleFonts.poppins(
-                                fontSize: 14,
-                                color: Colors.white.withOpacity(0.8),
+                            const SizedBox(height: 5),
+                            TextButton(
+                              onPressed: () {
+                                setState(() {
+                                  islogin=!islogin;
+                                });
+                              },
+                              child: Text(
+                                islogin?"don't have account? create account":"Already have an account? Log in here",
+                                style: GoogleFonts.poppins(
+                                  fontSize: 14,
+                                  color: Colors.white.withOpacity(0.8),
+                                ),
                               ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
--- lib/component/Display.dart
+++ lib/component/Display.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
 import 'package:guideconnect/component/Events.dart';
 import 'package:guideconnect/component/Lecture.dart';
@@ -5,7 +7,9 @@ import 'package:guideconnect/component/My_square.dart';
 
 
 class Display extends StatelessWidget {
+  // ignore: non_constant_identifier_names
   final String Ison;
+  // ignore: use_key_in_widget_constructors
   const Display(this.Ison);
 
   @override
