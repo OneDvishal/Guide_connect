8d0f707de502849a79e4ba99e6993271c9ded1dc
VivekPC <viveksahu1762@gmail.com>
2023-04-28 23:02:24 +0530

    Added delete button

--- lib/screen/edit_schedule_screen.dart
+++ lib/screen/edit_schedule_screen.dart
@@ -77,6 +77,12 @@ class _EditScheduleScreenState extends State<EditScheduleScreen> {
     });
   }
 
+  void _deleteSchedule(int index) {
+    setState(() {
+      lectures.removeAt(index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -127,6 +133,13 @@ class _EditScheduleScreenState extends State<EditScheduleScreen> {
                     ),
                     controller: TextEditingController(text: lectureTime),
                   ),
+                  trailing: IconButton(
+                    icon: const Icon(
+                      Icons.delete,
+                      color: Colors.red,
+                    ),
+                    onPressed: () => _deleteSchedule(index),
+                  ),
                 );
               },
             ),
@@ -144,7 +157,7 @@ class _EditScheduleScreenState extends State<EditScheduleScreen> {
                     ),
                   ),
                 ),
-                SizedBox(width: 10),
+                const SizedBox(width: 10),
                 Expanded(
                   child: TextField(
                     controller: newLectureTimeController,
@@ -167,4 +180,3 @@ class _EditScheduleScreenState extends State<EditScheduleScreen> {
     );
   }
 }
-
